3316bcab6a6f595fd0b1f6394d49e93920ec7b04
riteshbr <riteshritu1995@gmail.com>
2017-02-07 22:12:15 +0530

    added utf-8 to textwriter.py

--- Scrape/TextWriter.py
+++ Scrape/TextWriter.py
@@ -2,6 +2,6 @@ import csv
 
 def writeToCSV(text, symbol):
     fname = symbol + "_information.csv"
-    with open(fname, "w") as file:
+    with open(fname, "w", 'utf-8') as file:
         writer = csv.writer(file, delimiter = ';')
         writer.writerows(text)
