05f618fd04656baf06165384e9bf84932097f627
Halo Whiz <whizkid1236@gmail.com>
2017-02-07 21:25:23 +0530

    Formatted PriceExtractor

--- Scrape/PriceExtractor.py
+++ Scrape/PriceExtractor.py
@@ -1,11 +1,13 @@
 import csv
-i=1
-#change the path of historucal data downloaded .
-with open("HistoricalQuotes.csv","r") as source:
-    rdr= csv.reader( source )
-    with open("HistoricalPrices","w", newline='') as HistoricalPrices:
-        wtr= csv.writer(HistoricalPrices)
+
+
+i = 1
+#  change the path of historical data downloaded .
+with open("HistoricalQuotes.csv", "r") as source:
+    rdr = csv.reader(source)
+    with open("HistoricalPrices", "w", newline='') as HistoricalPrices:
+        wtr = csv.writer(HistoricalPrices)
         for r in rdr:
             if i > 2:
-                wtr.writerow( (r[0], r[1]) )
-            i=i+1;
+                wtr.writerow((r[0], r[1]))
+            i += 1
