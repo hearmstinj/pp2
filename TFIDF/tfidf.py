c65bfb07653434f0aeedbc3576dbda01ddca47fd
Halo Whiz <whizkid1236@gmail.com>
2017-02-11 17:01:42 +0530

    Lots of changes

--- TFIDF/tfidf.py
+++ TFIDF/tfidf.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import sklearn as sk
+import random as rd
 import csv
 from sklearn import feature_extraction
+from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB
 
 
 def build_df(path):
@@ -27,9 +30,19 @@ V = vectorizer.fit_transform(features.ravel()).todense()  # fit the features/ vo
 B = vectorizer.get_feature_names()
 doc1 = V[0].tolist()[0]
 words_used = [pair for pair in zip(range(0, len(doc1)), doc1) if pair[1] > 0]
-print(words_used)
+#print(words_used)
 #print(doc1)
-print(sorted(words_used, key=lambda t: t[1] * -1))
+'''print(sorted(words_used, key=lambda t: t[1] * -1))
 print(B[5460], B[1946], B[10559])
 print(V)
-print(len(B))
+print(len(B))'''
+
+a = [1 if rd.randint(0, 10) > 3 else 0 for x in range(371)]
+print(a)
+
+# GaussianNB part
+
+gnb = GaussianNB()
+#mnb = MultinomialNB()
+y_pred = gnb.fit(V[:272], a[:272]).predict(V[272:])
+print("Number of mislabelled points out of a total %d points is %d" % (len(V), (a[272:] != y_pred).sum()))
