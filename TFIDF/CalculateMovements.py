9baa9a0f73dd4a99d19797d9ae74794ed81ba2ae
riteshbr <riteshritu1995@gmail.com>
2017-02-18 22:58:46 +0530

    small change

--- TFIDF/CalculateMovements.py
+++ TFIDF/CalculateMovements.py
@@ -1,27 +1,28 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 18 18:45:30 2017
+
+@author: USER
+"""
 import csv
-
-
-def readfile(path):
-    file = pd.read_csv(path, header=0)
-    #print(file)
-    return file.as_matrix()
-
-
-def writefile(movements):
-    filename = "../CSV/movement.csv"
-    with open(filename, "w", encoding='utf-8') as f:
-        writer = csv.writer(f, delimiter=',')
-        writer.writerows(movements)
-
-data = readfile("../CSV/HistoricalQuotes.csv")
-
-movement = list()
-
-print(data[0])
-for it in range(1, len(data) - 1):
-    movement[data[it][0]] = 1 if data[it][2] > data[it + 1][2] else 0
-
-writefile(movement)
-
-
+prev=0
+i=0
+with open("../CSV/HistoricalQuotes.csv", 'r') as csvfile:
+     with open("../CSV/HistoricalQuotes1.csv", 'w')  as output:
+         reader = csv.reader(csvfile, delimiter=',')
+         writer = csv.writer(output)
+         for row in reader:
+             if i > 0:
+                 current=float(row[1])
+                 movement=prev-current
+                 prev=current
+                 if movement > 0:
+                     writer.writerow(row + [1])
+                 else:
+                     writer.writerow(row + [0])
+             i=i+1
+ #   movement[data[it][0]] = 1 if data[it][2] > data[it + 1][2] else 0
+#
+#writefile(movement)
+        
+    
