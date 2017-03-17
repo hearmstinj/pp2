dfd6d44c83e03788f9ee4faaa1c5e893e8912fa0
riteshbr <riteshritu1995@gmail.com>
2017-03-17 21:44:55 +0530

    generalising priceExtractor

--- Scrape/PriceExtractor.py
+++ Scrape/PriceExtractor.py
@@ -1,15 +1,17 @@
 import csv
 
-prev = 0
-with open('../CSV/HistoricalQuotes.csv', 'r') as csvfile:
-    with open('../CSV/QuoteMovements.csv', 'w') as output:
-        reader = csv.reader(csvfile, delimiter=',')
-        writer = csv.writer(output)
-        for row in reversed(list(reader)):
-            current = float(row[1])
-            movement = current-prev
-            prev = current
-            if movement > 0:
-                writer.writerow(row + [1])
-            else:
-                writer.writerow(row + [0])
+def getQuoteMovements(company):
+    prev = 0
+    with open('../CSV/' + company + 'HistoricalQuotes.csv', 'r') as csvfile:
+        with open('../CSV/'+ company + 'QuoteMovements.csv', 'w') as output:
+            reader = csv.reader(csvfile, delimiter=',')
+            writer = csv.writer(output)
+            for row in reversed(list(reader)):
+                current = float(row[1])
+                movement = current-prev
+                prev = current
+                if movement > 0:
+                    writer.writerow(row + [1])
+                else:
+                    writer.writerow(row + [0])
+                    
