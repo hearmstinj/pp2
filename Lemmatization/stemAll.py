5f96933948aebb8cf462e1d6fe6b675a237e8857
Halo Whiz <whizkid1236@gmail.com>
2017-03-17 22:20:19 +0530

    minor edit

---
+++ Lemmatization/stemAll.py
@@ -0,0 +1,3 @@
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+
--- web/statisticsdashboard/phpfiles/postToDashboard.php
+++ web/statisticsdashboard/phpfiles/postToDashboard.php
@@ -3,15 +3,11 @@
     $referrer = array();
     $labels = array();
     $pageScanned = fopen("CSV/lastpagescanned.csv", "r");
-    $i = 0;
     while(! feof($pageScanned)){
         $val = fgetcsv($pageScanned);
         array_push($referrer, $val[1]);
         array_push($labels, $val[0]);
-        $len = count($referrer);
     }
-
-    
 
     // Echo the json conversion of the array back to the dashboard
     $news = array("aapl" => $referrer[0], "amzn" => $referrer[1], "csco" => $referrer[2], "tsla" => $referrer[3], "msft" => $referrer[4], "ssnlf" => $referrer[5]);
