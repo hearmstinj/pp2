ca0d56a980b970e066f205e607a05897fb037f1a
Halo Whiz <whizkid1236@gmail.com>
2017-03-24 21:28:18 +0530

    stuff

--- Scrape/untitled0.py
+++ Scrape/untitled0.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 21 19:17:20 2017
-
-@author: USER
-"""
-
 import sys
+
 sys.path.append('../CSV/.')
 from bs4 import BeautifulSoup
 import urllib
@@ -16,38 +10,38 @@ import csv
 def getlastvalue(company):
     with open('../CSV/' + company + '_HistoricalQuotes.csv', 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        #print (next(reader)[0])
-        return (next(reader)[0])
-        
+        return next(reader)[0]
+
 def writetoHistoricalQuotes(lastdate, company):
-    url = "http://nasdaq.com/symbol/" + company + "/historical"
+    dict = {'Jan' : '01', 'Feb' : '02', 'Mar' : '03', 'Apr' : '04', 'May' : '05', 'Jun' : '06',
+        'Jul' : '07', 'Aug' : '08', 'Sep' : '09', 'Oct' : '10', 'Nov' : '11', 'Dec' : '12'}
+    url = "https://www.google.com/finance/historical?q=NASDAQ%3A"+company
     webpage = urllib.request.urlopen(url)
     soup = BeautifulSoup(webpage, 'lxml')
-    id1 = soup.find("div", {"id": "historicalContainer"})
-    print(id1)
-    id2 = id1.find("div", {"id" : "quotes_content_left_pnlAJAX"})
-    for table in id2:
-        print(table)
-    table = id1.find('table')   
-    print(table)
-    table_body = table.find('tbody')
-    print(table_body)
-    rows = table_body.find_all('tr')
-    for row in rows:
-        cells = row.find_all('td')
-        date = cells[0].find(text=True)
-        closingValue = cells[4].find(text=True)
-        volume = cells[5].find(text=True)
-        openingvalue = cells[1].find(text=True)
-        highValue = cells[2].find(text=True)
-        lowValue = cells[3].find(text=True) 
-        print (closingValue)
-            
-    #with open('../CSV' + company + 'HistoricalQuotes.csv', 'w') as csvfile:
-                
+    table = soup.find('table', {"class": "gf-table historical_price"})
+    data = table.stripped_strings
+    lst = list(data)
+    i = 6
+    while(i < len(lst)):
+        date = lst[i]
+        closingValue = lst[i+4]
+        Volume = lst[i+5]
+        openingValue = lst[i+1]
+        lowValue = lst[i+3]
+        highValue = lst[i+4]
+        month = date[0:3]
+        if(len(date) == 12):
+            year = date[8:12]
+            day = date[4:6]
+        else:
+            year = date[7:11]
+            day = date[4:5]
+        if(len(day) == 1):
+            actualDate = year+"/"+dict[month]+"/"+"0"+day
+        else:
+            actualDate = year+"/"+dict[month]+"/"+day
+        print(actualDate)
+        i = i+6
+        
 lastdate = getlastvalue('aapl')
-writetoHistoricalQuotes(lastdate, 'appl')        
-        
-        
-        
-        
+writetoHistoricalQuotes(lastdate, 'aapl')
